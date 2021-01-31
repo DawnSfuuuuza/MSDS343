88bbd465837f19493350682a0289e9d6f4a0e529
DawnSfuuuuza <33046510+DawnSfuuuuza@users.noreply.github.com>
2021-01-31 16:33:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    val = {"value": "Hello World!"}
+    return jsonify(val)
 
-@app.route('/newroute/<name>')
-def newroute(name):
-    """parameter"""
-    return "this was passed in: %s" % name
-
-
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
-
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
+   
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
