2b8d731fafdbf5f87123433e55e3b0fb1900e7d0
DawnSfuuuuza <33046510+DawnSfuuuuza@users.noreply.github.com>
2021-02-06 21:32:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 
 from flask import Flask, escape, request
 import pandas as pd
-import requests
-import io
 
 
-url = "https://github.com/DawnSfuuuuza/MSDS343/blob/main/Project.csv" # Make sure the url is the raw version of the file on GitHub
-download = requests.get(url).content
 
 # Reading the downloaded content and turning it into a pandas dataframe
 
-df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+df = pd.read_csv("https://github.com/DawnSfuuuuza/MSDS343/blob/main/Project.csv")
 
 # Printing out the first 5 rows of the dataframe
 
